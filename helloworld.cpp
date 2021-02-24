9ab7ee30d659a34b5de83af2a2f29ae8aeeed899
108061233 <aa0978505115@gmail.com>
2021-02-24 06:29:09 -0800

    add helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int main (void) {
+int main () {
     // print hello world for 9 times
     for (int i=1; i<10; ++i) {
-        cout << "hello world " << i << " times!\n";
+        printf ("hello world ");
+        printf ("%d", i);
+        printf (" times!\n");
     }
     return 0;
 }
